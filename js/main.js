fe91d0fe3c5da338994c0397cbadde278448b0ba
Tolebi Zhaksybai <52714747+tolebijaksybai@users.noreply.github.com>
2020-10-11 04:28:34 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+new WOW().init();
+
+const cartBtn = document.querySelector('.cart_btn'),
+        modal = document.querySelector('.modal'),
+        closeBtn = document.querySelector('.btn_close'),
+        cancelBtn = document.querySelector('.btn_cancel');
+
+cartBtn.addEventListener('click',()=>{
+    modal.classList.add('modal_show');
+});
+closeBtn.addEventListener('click', ()=>{
+    modal.classList.remove('modal_show');
+});
+cancelBtn.addEventListener('click', ()=>{
+    modal.classList.remove('modal_show');
+})
